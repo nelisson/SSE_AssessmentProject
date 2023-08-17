fb80379f14c45a8d96c6d3d95995e42cc5af3517
nelisson <nelisson.ad@gmail.com>
2023-08-17 11:32:03 -0300

    feat: mouse position

---
+++ SSE_AssessmentProject/wwwroot/js/mouse-position.js
@@ -0,0 +1,6 @@
+﻿document.addEventListener('mousemove', function (event) {
+    const mouseX = event.clientX;
+    const mouseY = event.clientY;
+    document.getElementById('mouseX').innerText = mouseX;
+    document.getElementById('mouseY').innerText = mouseY;
+});
